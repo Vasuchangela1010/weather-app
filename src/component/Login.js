c8def941195c81ed7683ebe955cb91057db688fb
Vasuchangela1010 <92933406+Vasuchangela1010@users.noreply.github.com>
2022-02-10 23:34:32 +0530

    Update Login.js

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,15 +1,11 @@
 import { useState } from "react";
 import { useHistory } from "react-router";
 export default function Login() {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const history=useHistory();
   const onChangeHandler = (fieldName, value)=>{
-    if(fieldName === "name"){
-      setName(value);
-    }
-    else if(fieldName==="email"){
+    if(fieldName==="email"){
       setEmail(value);
     }
    else if(fieldName === "password"){
@@ -18,13 +14,12 @@ export default function Login() {
   }
   const onSubmitHandler = (e)=>{
     e.preventDefault();
-    if(name.trim()==="" || email.trim() ==="" || password===""){
+    if(email.trim() ==="" || password===""){
       alert("required all fields");
     }
     else{
       e.preventDefault()
       history.push('/Logout');
-      setName("");
       setEmail("");
       setPassword("");
   }
@@ -32,15 +27,15 @@ export default function Login() {
   return (
     <div className="App">
     <div className="header"><h1>Enter details</h1></div>
+    <div className="details">
       <form onSubmit={(e)=>{onSubmitHandler(e)}}>
-      <label for="name"><b>Name:    </b> </label>
-        <input type="text" value={name} onChange={(e)=>{ onChangeHandler("name",e.target.value)}} /> <br/><br/>
-        <label for="email"><b>Email:     </b></label>
+        <label for="email"><b>Email:     </b></label><br/>
          <input type="email"  value={email} onChange={(e)=>{ onChangeHandler("email",e.target.value)}} /> <br/><br/>
-         <label for="password"><b>Password:     </b></label>
+         <label for="password"><b>Password:     </b></label><br/>
          <input type="password" value={password} onChange={(e)=>{ onChangeHandler("password",e.target.value)}} /><br/> <br/>
          <button type="submit">Login</button>
         </form>
+        </div>
     </div>
   );
 }
